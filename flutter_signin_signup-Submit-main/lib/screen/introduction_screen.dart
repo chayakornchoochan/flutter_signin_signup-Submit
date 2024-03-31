6774c20f45bfc0d53a744fe67cb9ec1593b0659c
chayakornchoochan <159178193+chayakornchoochan@users.noreply.github.com>
2024-04-01 01:34:31 +0700

    Add files via upload

---
+++ flutter_signin_signup-Submit-main/lib/screen/introduction_screen.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'home_screen.dart';
+
+class IntroScreen extends StatelessWidget {
+  IntroScreen({super.key});
+  
+  final List<PageViewModel> pages = [
+    PageViewModel(
+      title: 'Lorem',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      footer: SizedBox(
+        height: 45,
+        width: 150,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            elevation: 8,
+          ),
+          onPressed: () {},
+          child: const Text('Hi lorem'),
+        ),
+      ),
+      image: Image.network(
+          'https://www.lipsum.com/images/banners/black_234x60.gif'),
+      decoration: const PageDecoration(
+        titleTextStyle: TextStyle(
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    ),
+    PageViewModel(
+      title: 'Lorem',
+      body:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      footer: SizedBox(
+        height: 45,
+        width: 150,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            elevation: 8,
+          ),
+          onPressed: () {},
+          child: const Text('Html'),
+        ),
+      ),
+      image: Image.network(
+          'https://www.lipsum.com/images/banners/black_234x60.gif'),
+      decoration: const PageDecoration(
+        titleTextStyle: TextStyle(
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    ),
+    PageViewModel(
+      title: 'Lorem',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      footer: SizedBox(
+        height: 45,
+        width: 150,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            elevation: 8,
+          ),
+          onPressed: () {},
+          child: const Text('java'),
+        ),
+      ),
+      image: Image.network(
+          'https://www.lipsum.com/images/banners/black_234x60.gif'),
+      decoration: const PageDecoration(
+        titleTextStyle: TextStyle(
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Onboarding'),
+        backgroundColor: Color.fromARGB(255, 48, 55, 244),
+        centerTitle: true,
+      ),
+      body: IntroductionScreen(
+        globalBackgroundColor: Color.fromARGB(255, 116, 227, 237),
+        pages: pages,
+        showSkipButton: true,
+        skip: const Text('Skip'),
+        showDoneButton: true,
+        done: const Text('Done'),
+        showNextButton: true,
+        next: const Icon(Icons.arrow_forward, size: 25),
+        onDone: () => onDone(context),
+        curve: Curves.bounceOut,
+        dotsDecorator: const DotsDecorator(
+          size: Size(15, 15),
+          color: Color.fromARGB(255, 215, 0, 0),
+          activeColor: Color.fromARGB(255, 243, 203, 238),
+          activeSize: Size.square(20),
+        ),
+      ),
+    );
+  }
+
+  void onDone(context) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('ON_BOARDING', false);
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => HomeScreen(),
+      ),
+    );
+  }
+}
